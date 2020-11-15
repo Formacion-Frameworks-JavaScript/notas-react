936557b4030c72f01d091dd0c3627fa3a357062f
Mario <mario@mariogl.com>
2020-11-15 07:44:59 +0100

    Componente NuevaNota

--- src/App.js
+++ src/App.js
@@ -1,36 +1,18 @@
 import { Navegacion } from "./componentes/Navegacion";
+import { NuevaNota } from "./componentes/NuevaNota";
 
 const { Loading } = require("./componentes/Loading");
 
 function App() {
   const cargando = false;
+  const muestraFormulario = false;
   return (
     <>
       <Navegacion />
       <main className="container">
         <div className="row form-nueva">
           <div className="col-12 col-lg-8 offset-lg-2">
-            <div className="card">
-              <div className="card-header">
-                <span>Nueva nota aula A</span>
-                <i className="fa fa-minus-circle"></i>
-              </div>
-              <form className="card-body">
-                <div className="form-group">
-                  <label htmlFor="nombre">Nombre:</label>
-                  <input type="text" className="form-control" id="nombre" required />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="apellidos">Apellidos:</label>
-                  <input type="text" className="form-control" id="apellidos" required />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="nota">Nota:</label>
-                  <input type="number" className="form-control form-control-sm" id="nota" required />
-                </div>
-                <button className="btn btn-primary">añadir</button>
-              </form>
-            </div>
+            {muestraFormulario && <NuevaNota />}
           </div>
         </div>
         <div className="row">
--- src/componentes/NuevaNota.js
+++ src/componentes/NuevaNota.js
@@ -1,5 +1,25 @@
 export const NuevaNota = props => {
     return (
-        <></>
+        <div className="card">
+            <div className="card-header">
+                <span>Nueva nota aula A</span>
+                <i className="fa fa-minus-circle"></i>
+            </div>
+            <form className="card-body">
+                <div className="form-group">
+                    <label htmlFor="nombre">Nombre:</label>
+                    <input type="text" className="form-control" id="nombre" required />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="apellidos">Apellidos:</label>
+                    <input type="text" className="form-control" id="apellidos" required />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="nota">Nota:</label>
+                    <input type="number" className="form-control form-control-sm" id="nota" required />
+                </div>
+                <button className="btn btn-primary">añadir</button>
+            </form>
+        </div>
     )
 }
